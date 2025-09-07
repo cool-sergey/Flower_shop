bb45a9a1558f68ba11bc6bc06e7c6cd5661a7ab0
cool-sergey <sergey.mixalyuk@gmail.com>
2025-09-07 23:30:12 +0300

    add new model for bucket

--- new_project/example/models.py
+++ new_project/example/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 class FlowerType(models.Model):
     name = models.CharField(max_length = 128, unique = True)
@@ -15,6 +16,16 @@ class Product(models.Model):
     type = models.ForeignKey(to = FlowerType, on_delete = models.CASCADE )
     def __str__(self):
         return f"{self.name} | category: {self.type.name}"
+    
+class Bucket(models.Model):
+    user = models.ForeignKey(to = User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to = Product, on_delete = models.CASCADE)
+    quantity = models.PositiveIntegerField(default = 0)
+    created_timestamp = models.DateTimeField(auto_now_add = True)
+    def __str__(self):
+        return f'Пользователь {self.user.email} | Продукт: {self.product.name} '
+    
+
 ## у нас есть фикстуры, это типо экзмепляр
 # 
 #  базы данных, которую мы можем загрузить при помощи команды
