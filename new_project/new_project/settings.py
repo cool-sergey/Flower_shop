83cae6ebabb3b04750906d31c60243bdc1e8116e
cool-sergey <sergey.mixalyuk@gmail.com>
2025-09-18 13:44:25 +0300

    change DB to Postgre

--- new_project/new_project/settings.py
+++ new_project/new_project/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'new_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'new_django_project',
+        'USER': 'django_admin',
+        'PASSWORD': 'zxcserg20',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
